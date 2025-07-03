5ccc0418604d6ba4109ef7e3d526c71046c12050
Shamika Earle <71336412+Shamikae@users.noreply.github.com>
2025-07-03 13:33:22 -0400

    Fix React mounting and script loading errors

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import React, { useState } from 'react';
-import './App.css';
+// eslint-disable-next-line strict
+"use strict";
 
-function App() {
-  const [adviceId, setAdviceId] = useState('');
-  const [adviceText, setAdviceText] = useState('Click the button to get an affirmation!');
-  const [isLoading, setIsLoading] = useState(false);
+const diceBtn = document.querySelector(".button-section");
+const adviceNumber = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".quotes");
+const apiUrl = "https://api.adviceslip.com/advice";
 
-  const apiUrl = "https://api.adviceslip.com/advice";
-
-  const fetchAdvice = () => {
-    setIsLoading(true);
-    fetch(apiUrl, { cache: "no-cache" })
-      .then((response) => response.json())
-      .then((response) => {
-        let data = response.slip;
-        let dataId = data.id;
-        let dataAdvice = data.advice;
-        
-        setAdviceId(dataId);
-        setAdviceText(dataAdvice);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching advice:', error);
-        setAdviceText('Sorry, could not fetch advice. Please try again.');
-        setIsLoading(false);
-      });
-  };
-
-  return (
-    <div className="App">
-      <div className="advice-container">
-        <div className="advice-number">
-          {adviceId && `Affirmation #${adviceId}`}
-        </div>
-        <div className="quotes">
-          {isLoading ? 'Loading...' : adviceText}
-        </div>
-        <div className="button-section" onClick={fetchAdvice}>
-          <button disabled={isLoading}>
-            Get New Affirmation
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+//Button click event
+diceBtn.addEventListener("click", () => {
+  //Request Data
+  fetch(apiUrl, {cache: "no-cache"})
+    .then((response) => response.json())
+    .then((response) => {
+      let data = response.slip;
+      let dataId = data.id;
+      let dataAdvice = data.advice;
+      //Inject to DOM
+      adviceNumber.innerHTML = `Affirmation #${dataId}`;
+      adviceText.innerHTML = dataAdvice;
+    });
+});
